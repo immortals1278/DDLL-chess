7e027447c2cfd59b172139dc3daac6c1133257c0
immortals1278 <3156160490@qq.com>
2026-02-15 18:21:28 +0800

    按钮开始游戏

--- server_scripts/01_events.js
+++ server_scripts/01_events.js
@@ -7,12 +7,28 @@ PlayerEvents.loggedIn(event => {
         Game.players.push(player.uuid)
     }
 
-    if (!Game.started) {
-    player.server.scheduleInTicks(1, () => {
-        startGame(player.level, player)
-    })
-}
+    
 
+})
+
+//按钮开始游戏
+BlockEvents.rightClicked(event => {
+
+    if (Game.started) return
+
+    const player = event.player
+    const level = event.level
+    const block = event.block
+
+    if (!block.id.includes("button")) return
+
+    const pos = block.pos
+
+    if (pos.x == 0 && pos.y == -60 && pos.z == 0) {
+
+        startGame(level, player)
+        level.runCommandSilent("say 游戏正式开始")
+    }
 })
 
 
@@ -21,7 +37,11 @@ PlayerEvents.loggedIn(event => {
 ItemEvents.rightClicked(event => {
 
     const player = event.player
-    if (!Game.started) return
+    if (!Game.started) {
+        player.tell("§c游戏还没开始")
+        return
+    }
+
 
     if (!event.item.is("minecraft:compass")) return
 
@@ -36,61 +56,4 @@ ItemEvents.rightClicked(event => {
 })
 
 
-// 每tick监听移动
-PlayerEvents.tick(event => {
-
-    if (!Game.started) return
-
-    const player = event.player
-    const currentUUID = Game.players[Game.turnIndex]
-
-    if (player.uuid != currentUUID) return
-
-    const piece = Game.pieces[player.uuid]
-    if (!piece) return
-
-    let moved = false
-
-    // 前进（W）
-    if (player.forward > 0) {
-        piece.z--
-        moved = true
-    }
-
-    // 后退（S）
-    if (player.forward < 0) {
-        piece.z++
-        moved = true
-    }
-
-    // 左（A）
-    if (player.strafe > 0) {
-        piece.x--
-        moved = true
-    }
-
-    // 右（D）
-    if (player.strafe < 0) {
-        piece.x++
-        moved = true
-    }
-
-    if (!moved) return
-
-    piece.x = Math.max(0, Math.min(Game.board.width - 1, piece.x))
-    piece.z = Math.max(0, Math.min(Game.board.height - 1, piece.z))
-
-    const worldX = Game.board.originX + piece.x + 0.5
-    const worldY = Game.board.originY + 1
-    const worldZ = Game.board.originZ + piece.z + 0.5
-
-    player.server.runCommandSilent(
-        `tp @e[type=minecraft:armor_stand,name="${player.name.string}"] ${worldX} ${worldY} ${worldZ}`
-    )
-
-    Game.turnIndex++
-    if (Game.turnIndex >= Game.players.length) {
-        Game.turnIndex = 0
-    }
-})
 
